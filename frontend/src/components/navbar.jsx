1df5317099bffe987b2388b30d30f9d2cbfb311b
pranav paul <pranav2006paul@gmail.com>
2025-09-27 20:12:29 +0530

    understanding the branch functions

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
         <div className="flex space-x-4 items-center ">
           <img src=".\src\images\estateease-logo.png" className="h-20 w-20" />
           <Link to="/">
-            <p className="font-bold text-2xl text-white">EateEase</p>
+            <p className="font-bold text-2xl text-white">EstateEase</p>
           </Link>
         </div>
         <a href="#" className="navlink">
